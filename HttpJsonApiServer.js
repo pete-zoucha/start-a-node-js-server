e3b05d5f4ff717b72cc4b60ec01b987ca8d212ed
Pete Zoucha <pete.zoucha@gmail.com>
2018-01-08 20:49:20 +0000

    All done with learnyounode!

--- HttpJsonApiServer.js
+++ HttpJsonApiServer.js
@@ -6,15 +6,21 @@ var port = process.argv[2];
 var server = http.createServer(function (request, response) {
     //console.log(request.url);
     var urlInfo = url.parse(request.url);
-    console.log(Date(urlInfo.query));
+    //console.log(Date.parse(urlInfo.query.replace("iso=","")));
     response.writeHead(200, { 'Content-Type': 'application/json' });
+    var resObj = undefined;
     if (urlInfo.pathname === '/api/parsetime') {
-        
+        var date = new Date(Date.parse(urlInfo.query.replace("iso=","")));
+        resObj = { 
+                    "hour": date.getHours(), 
+                    "minute": date.getMinutes(), 
+                    "second": date.getSeconds() 
+                };
     }
     if (urlInfo.pathname === '/api/unixtime') {
-        
+        resObj = { "unixtime": Date.parse(urlInfo.query.replace("iso=","")) };
     }
-    response.write(resObj, function() {
+    response.write(JSON.stringify(resObj),"UTF-8",function() {
         response.end();
     });
 });
